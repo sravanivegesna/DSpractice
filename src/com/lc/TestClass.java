2e56545a12d4992683de786c5c526cddc92abb6e
svegesna <svegesna@inmoment.com>
2022-12-09 18:31:09 -0700

    This is a first commit

---
+++ src/com/lc/TestClass.java
@@ -0,0 +1,8 @@
+package com.lc;
+
+public class TestClass {
+	public static void main(String a[]) {
+		System.out.println("Hi This is a test ");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sravanivegesna
+ *
+ */
+module LeetCodePractice {
+}
